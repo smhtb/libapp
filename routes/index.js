eb66ba013d467606ad595bc5c83d0701f0a18b89
smhtb <m.tababala@gmail.com>
2020-03-04 13:30:49 +0330

    Add prefix '/api/v1' to all routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const homeRouter = require("./homeRouter");
 const userRouter = require("./userRouter");
 const authRouter = require("./authRouter");
+const bookRouter = require("./bookRouter");
 const guard = require("../middlewares/guard");
 
 
 module.exports = (app) => {
     app.use('/', homeRouter);
-    app.use('/auth', authRouter);
-    app.use('/user', guard, userRouter);
+    app.use('/api/v1/auth', authRouter);
+    app.use('/api/v1/user', userRouter);
+    app.use('/api/v1/book', guard, bookRouter);
 };
