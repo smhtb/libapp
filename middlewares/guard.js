5c6d345b255d82958ddb862616ba5f9e8b73a9a2
smhtb <m.tababala@gmail.com>
2020-03-04 17:32:53 +0330

    Guard check that jwttoken exist in user document in db

--- middlewares/guard.js
+++ middlewares/guard.js
@@ -1,19 +1,41 @@
 const jwt = require("jsonwebtoken");
 const privateKey = require("../config/config").secretKey;
+const User = require("../models/User");
 
 module.exports = (req, res, next) => {
     const { jwttoken } = req.headers;
     if(!jwttoken) {
-        res.send("Authentication error.")
+        res.status(400);
+        res.send({
+            status: false,
+            message: "Please provide jwttoken in your request header.",
+            data: {}
+        });
     }
     else {
-        jwt.verify(jwttoken, privateKey, function(error, verified) {
+        jwt.verify(jwttoken, privateKey, async function(error, verified) {
             if(error) {
-                res.send("Authentication error.")
+                res.status(400);
+                return res.send({
+                    status: false,
+                    message: "Your token could not be verified.",
+                    data: {}
+                });
             }
             if(verified) {
-                req.userid = verified.id;
-                next();
+                const findedUser = await User.findById(verified.id);
+                if(findedUser.token != jwttoken) {
+                    res.status(400);
+                    return res.send({
+                        status: false,
+                        message: "Your token could not be verified.",
+                        data: {}
+                    });                    
+                }
+                else {
+                    req.userid = verified.id;
+                    next();
+                }
             }
         })
     }    
